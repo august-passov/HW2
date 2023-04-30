33ec91c6042da88c80757d114da8bc50604bd067
august passov <gus.passov@kellogg.northwestern.edu>
2023-04-30 16:24:46 +0000

    HW2 in Progress

--- kmdb.rb
+++ kmdb.rb
@@ -80,17 +80,21 @@ Role.destroy_all
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 
+# Movie Code
+
 new_studio = Studio.new
 
 new_studio["name"] = "Warner Bros."
 puts new_studio.save
+
+warner = Studio.find_by({"name" => "Warner Bros."})
 
 batman_begins = Movie.new
 
 batman_begins["title"] = "Batman Begins"
 batman_begins["year_released"] = "2005"
 batman_begins["rated"] = "PG-13"
-batman_begins["studio_id"] = new_studio["id"]
+batman_begins["studio_id"] = warner["id"]
 puts batman_begins.save
 
 dark_knight = Movie.new
@@ -98,7 +102,7 @@ dark_knight = Movie.new
 dark_knight["title"] = "The Dark Knight"
 dark_knight["year_released"] = "2008"
 dark_knight["rated"] = "PG-13"
-dark_knight["studio_id"] = new_studio["id"]
+dark_knight["studio_id"] = warner["id"]
 puts dark_knight.save
 
 knight_rises = Movie.new
@@ -106,42 +110,75 @@ knight_rises = Movie.new
 knight_rises["title"] = "The Dark Rises"
 knight_rises["year_released"] = "2012"
 knight_rises["rated"] = "PG-13"
-knight_rises["studio_id"] = new_studio["id"]
+knight_rises["studio_id"] = warner["id"]
 puts knight_rises.save
 
-bb_actors = Actor.new
+# Actor Code
 
-bb_actors["name"] = "Christian Bale"
-bb_actors["name"] = "Michael Caine"
-bb_actors["name"] = "Liam Neeson"
-bb_actors["name"] = "Katie Holmes"
-bb_actors["name"] = "Gary Oldman"
-puts bb_actors.save
+actor1 = Actor.new
+actor1["name"] = "Christian Bale"
+puts actor1.save
 
-dk_actors = Actor.new
+actor2 = Actor.new
+actor2["name"] = "Michael Caine"
+puts actor2.save
 
-dk_actors["name"] = "Christian Bale"
-dk_actors["name"] = "Heath Ledger"
-dk_actors["name"] = "Aaron Eckhart"
-dk_actors["name"] = "Maichael Caine"
-dk_actors["name"] = "Maggie Gyllenhaal"
-puts dk_actors.save
+actor3 = Actor.new
+actor3["name"] = "Liam Neeson"
+puts actor3.save
 
-dkr_actors = Actor.new
+actor4 = Actor.new
+actor4["name"] = "Katie Holmes"
+puts actor4.save
 
-dkr_actors["name"] = "Christian Bale"
-dkr_actors["name"] = "Gary Oldman"
-dkr_actors["name"] = "Tom Hardy"
-dkr_actors["name"] = "Joseph Gordon-Levitt"
-dkr_actors["name"] = "Anne Hathaway"
-puts dkr_actors.save
+actor5 = Actor.new
+actor5["name"] = "Gary Oldman"
+puts actor5.save
 
-# bb_roles1 = Role.new
+actor6 = Actor.new
+actor6["name"] = "Heath Ledger"
+puts actor6.save
 
-# bb_roles["movie_id"] = batman_begins["id"]
-# bb_roles["actor_id"] = bb_actors["id"]
-# bb_roles["character_name"] = "Bruce Wayne"
-# puts bb_roles1.save
+actor7 = Actor.new
+actor7["name"] = "Aaron Eckhart"
+puts actor7.save
+
+actor8 = Actor.new
+actor8["name"] = "Maggie Gyllenhaal"
+puts actor8.save
+
+actor9 = Actor.new
+actor9["name"] = "Tom Hardy"
+puts actor9.save
+
+actor10 = Actor.new
+actor10["name"] = "Joseph Gordon-Levitt"
+puts actor10.save
+
+actor11 = Actor.new
+actor11["name"] = "Anne Hathaway"
+puts actor11.save
+
+# Character Code
+bbegins = Movie.find_by({"title" => "Batman Begins"})
+bale = Actor.find_by({"name" => "Christian Bale"})
+
+bb_role1 = Role.new
+
+bb_role1["movie_id"] = bbegins["id"]
+bb_role1["actor_id"] = bale["id"]
+bb_role1["character_name"] = "Bruce Wayne"
+puts bb_role1.save
+
+bbegins = Movie.find_by({"title" => "Batman Begins"})
+caine = Actor.find_by({"name" => "Michael Caine"})
+
+bb_role2 = Role.new
+
+bb_role2["movie_id"] = bbegins["id"]
+bb_role2["actor_id"] = caine["id"]
+bb_role2["character_name"] = "Alfred"
+puts bb_role2.save
 
 
 # Prints a header for the movies output
@@ -150,7 +187,8 @@ puts "======"
 puts ""
 
 # Query the movies data and loop through the results to display the movies output.
-movie_studio = Movie.where({"studio_id" => new_studio["id"]})
+# warner = Studio.find_by({"name" => "Warner Bros."})
+movie_studio = Movie.where({"studio_id" => warner["id"]})
 
 for movie in movie_studio
     title = movie["title"]
@@ -161,11 +199,26 @@ for movie in movie_studio
 end
 
 # Prints a header for the cast output
-# puts ""
-# puts "Top Cast"
-# puts "========"
-# puts ""
+puts ""
+puts "Top Cast"
+puts "========"
+puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
-# bb_movie = Movie.where({"name" =>["Batman Begins"]})
-# for movie in 
+# begins = Role.where({"movie_id" => bbegins["id"]})
+all_roles = Role.all
+for role in all_roles
+    title = role["movie_id"]
+    actor = role["actor_id"]
+    character = role["character_name"]
+    puts "#{title} #{actor} #{character}"
+end
+
+# for cast in begins
+#     title = begins["movie_id"]
+#     actor = begins["actor_id"]
+#     character = begins["character_name"]
+#     puts "#{title} #{actor} #{character}"
+# end
+
+
